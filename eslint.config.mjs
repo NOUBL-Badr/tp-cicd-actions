60454d78e0b3054e29a16d2134ff9d6561d04198
NOUBL-Badr <badr.noubl123@gmail.com>
2026-04-21 14:37:38 +0100

    chore: add ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,21 @@
-// eslint.config.mjs
-export default [
-{
-files: ['src/**/*.js'],
-rules: {
-'no-unused-vars': 'warn',
-'no-console': 'warn',
-'eqeqeq': 'error',
-'no-var': 'error',
+import globals from "globals";
 
-'prefer-const': 'warn',
-},
-},
+export default [
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+        ecmaVersion: "latest",
+        sourceType: "commonjs", // Précise qu'on utilise module.exports
+        globals: {
+            ...globals.node // Ajoute les variables globales de Node (module, process, etc.)
+        }
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'eqeqeq': 'error',
+      'no-var': 'error',
+      'prefer-const': 'warn',
+    },
+  },
 ];
